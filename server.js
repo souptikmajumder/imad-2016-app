58d5a18eee8d21cb44b119cc0057f7b7caf63721
souptikmajumder <soujd.2012@gmail.com>
2016-11-03 16:02:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+// app.get('/ui/page1.html', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+// });
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
