2b05e0146ee81d4418cc150a10eda67faf5f06a6
souptikmajumder <soujd.2012@gmail.com>
2016-11-04 01:30:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ app.get('/ui/home.gif', function (req, res) {
 
 
 
-app.get('/ui/pic3.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'pic3.jpg'));
+app.get('/ui/tweet.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'tweeet.jpg'));
 });
-app.get('/ui/pic2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'pic2.jpg'));
+app.get('/ui/insta.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'insta.jpg'));
 });
-app.get('/ui/pic5.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'pic5.jpg'));
+app.get('/ui/linkedin.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'linkedin.jpg'));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
